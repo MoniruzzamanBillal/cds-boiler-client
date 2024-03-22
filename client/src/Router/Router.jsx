a9591470b59249dd89c6bde0bd3cd3fcfd38d336
Md. Moniruzzaman <mdmoniruzzamanbillal@gmail.com>
2024-03-23 00:51:34 +0600

    dashboard added

--- client/src/Router/Router.jsx
+++ client/src/Router/Router.jsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import RootPage from "./RootPage";
 import Home from "../Pages/Home";
+import Dashboard from "../Pages/Dashboard";
 
 export const Routes = createBrowserRouter([
   {
@@ -10,6 +11,16 @@ export const Routes = createBrowserRouter([
       {
         path: "/",
         element: <Home />,
+      },
+    ],
+  },
+  {
+    path: "/dashboard",
+    element: <Dashboard />,
+    children: [
+      {
+        path: "/dashboard",
+        element: <p>Setting routes</p>,
       },
     ],
   },
